529d8b80f268bf7faddcda2eadac31287a2a293c
Hannes Holst <35895664+HannesHolst1@users.noreply.github.com>
2021-04-20 10:31:44 +0200

    clean config.py

---
+++ backend/config.py
@@ -0,0 +1,14 @@
+mongodb = {
+  "uri": "",
+  "database": "",
+  "request_collection": "requests",
+  "tweets_collection": "tweets",
+  "media_collection": "media",
+  "users_collection": "users"
+}
+twitter = {
+  "bearer": "",
+  "tweet_search_uri": "https://api.twitter.com/2/tweets/search/recent?",
+  "oembed": "https://publish.twitter.com/oembed?url=",
+  "twitter_url_root": "https://twitter.com/"  
+}
